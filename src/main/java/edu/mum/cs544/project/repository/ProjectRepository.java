578c2e981e0417936408700e225491b2d084381c
Doan Danh Dat <doandanhdat852004@gmail.com>
2018-04-21 11:46:11 -0500

    Complete the search functionality on the home page

--- src/main/java/edu/mum/cs544/project/repository/ProjectRepository.java
+++ src/main/java/edu/mum/cs544/project/repository/ProjectRepository.java
@@ -2,12 +2,9 @@ package edu.mum.cs544.project.repository;
 
 import java.util.Date;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import edu.mum.cs544.project.model.Project;
-import edu.mum.cs544.project.model.ProjectStatusEnum;
 
 
 /**
--- src/main/java/edu/mum/cs544/project/repository/ProjectSearchRepository.java
+++ src/main/java/edu/mum/cs544/project/repository/ProjectSearchRepository.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 import edu.mum.cs544.project.model.Project;
+import edu.mum.cs544.project.model.ProjectStatusEnum;
 import edu.mum.cs544.project.utils.SearchProjectParam;
 
 @Repository
@@ -25,43 +26,30 @@ public class ProjectSearchRepository {
     if (!StringUtils.isEmpty(params.getName())) {
       sql += " and LOWER(p.name) like CONCAT('%', LOWER(:name), '%')";
     }
-    // if (!StringUtils.isEmpty(genreShow) && !"ALL".equals(genreShow)) {
-    // sql += " and tv.genre = :genreShow";
-    // }
-    // if (ratingShow >= 1) {
-    // sql += " and tv.rating = :ratingShow";
-    // }
-    // if (!StringUtils.isEmpty(nameArtis)) {
-    // sql += " and LOWER(artist.name) like CONCAT('%', LOWER(:nameArtis), '%')";
-    // }
-    // if (!StringUtils.isEmpty(nameCharacter)) {
-    // sql += " and LOWER(character.name) like CONCAT('%', LOWER(:nameCharacter), '%')";
-    // }
-    // if (!StringUtils.isEmpty(nameDirector)) {
-    // sql += " and LOWER(director.name) like CONCAT('%', LOWER(:nameDirector), '%')";
-    // }
-    //
-    // TypedQuery<TvShow> query = em.createQuery(sql, TvShow.class);
-    //
-    // if (!StringUtils.isEmpty(nameShow)) {
-    // query.setParameter("nameShow", nameShow);
-    // }
-    // if (!StringUtils.isEmpty(genreShow) && !"ALL".equals(genreShow)) {
-    // query.setParameter("genreShow", TvShow.Genre.valueOf(genreShow));
-    // }
-    // if (ratingShow >= 1) {
-    // query.setParameter("genreShow", genreShow);
-    // }
-    // if (!StringUtils.isEmpty(nameArtis)) {
-    // query.setParameter("nameArtis", nameArtis);
-    // }
-    // if (!StringUtils.isEmpty(nameCharacter)) {
-    // query.setParameter("nameCharacter", nameCharacter);
-    // }
-    // if (!StringUtils.isEmpty(nameDirector)) {
-    // query.setParameter("nameDirector", nameDirector);
-    // }
+    if (!StringUtils.isEmpty(params.getStatus()) && !"ALL".equals(params.getStatus())) {
+      sql += " and p.status = :status";
+    }
+    if (!StringUtils.isEmpty(params.getLocation())) {
+      sql += " and LOWER(p.location) like CONCAT('%', LOWER(:location), '%')";
+    }
+    if (!StringUtils.isEmpty(params.getSkills())) {
+      sql += " and s.id IN :skills";
+    }
+
     TypedQuery<Project> query = em.createQuery(sql, Project.class);
+
+    if (!StringUtils.isEmpty(params.getName())) {
+      query.setParameter("name", params.getName());
+    }
+    if (!StringUtils.isEmpty(params.getStatus()) && !"ALL".equals(params.getStatus())) {
+      query.setParameter("status", ProjectStatusEnum.valueOf(params.getStatus()));
+    }
+    if (!StringUtils.isEmpty(params.getLocation())) {
+      query.setParameter("location", params.getLocation());
+    }
+    if (!StringUtils.isEmpty(params.getSkills())) {
+      query.setParameter("skills", params.getSkills());
+    }
     return query.getResultList();
   }
 }
