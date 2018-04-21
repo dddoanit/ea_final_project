e7daf86e138474c3f07ff1593a2466d4b9882e39
Doan Danh Dat <doandanhdat852004@gmail.com>
2018-04-21 14:07:50 -0500

    update the project detail page

--- src/main/java/edu/mum/cs544/project/controller/ProjectDetailController.java
+++ src/main/java/edu/mum/cs544/project/controller/ProjectDetailController.java
@@ -1,23 +1,37 @@
 package edu.mum.cs544.project.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import edu.mum.cs544.project.model.Project;
+import edu.mum.cs544.project.model.ProjectSkill;
+import edu.mum.cs544.project.model.Skill;
 import edu.mum.cs544.project.service.ProjectService;
 
 @Controller
 @RequestMapping("/project")
 public class ProjectDetailController {
 
-	@Autowired
-	  private ProjectService projectService;
-	 @RequestMapping(value = "/details/{id}", method = RequestMethod.GET)
-	  public String showDetails(Model model, @PathVariable("id") int id) {
-	   model.addAttribute(projectService.findById(id));
-	    return "project-details";
-	  }
+  @Autowired
+  private ProjectService projectService;
+
+  @RequestMapping(value = "/details/{id}", method = RequestMethod.GET)
+  public String showDetails(Model model, @PathVariable("id") int id) {
+    Project project = projectService.findById(id);
+    model.addAttribute("project", project);
+    List<Skill> skills = new ArrayList<>();
+    
+    for (ProjectSkill e: project.getProjectSkills()) {
+      Skill addedSkill = e.getSkill();
+      addedSkill.setNumRes(e.getNumResource());
+      skills.add(addedSkill);
+    }
+    model.addAttribute("skills", skills);
+    return "project-details";
+  }
 }
