0f84b618447e2c98258c09242abc5a85a3bafd48
Doan Danh Dat <doandanhdat852004@gmail.com>
2018-04-23 10:09:33 -0500

    format code

--- src/main/java/edu/mum/cs544/project/service/UserService.java
+++ src/main/java/edu/mum/cs544/project/service/UserService.java
@@ -3,12 +3,10 @@ package edu.mum.cs544.project.service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import edu.mum.cs544.project.config.RabbitMqConfig;
 import edu.mum.cs544.project.model.Project;
 import edu.mum.cs544.project.model.Role;
@@ -21,7 +19,7 @@ public class UserService {
 
   @Autowired
   private UserRepository userRepository;
-  
+
   @Autowired
   private RabbitTemplate rabbitTemplate;
 
@@ -54,63 +52,56 @@ public class UserService {
   public List<User> findBySkill(int skillId) {
     return userRepository.findBySkill(skillId);
   }
-  
+
   public void sendProjectMessage(int skillId, Project project) {
     List<User> users = userRepository.findBySkill(skillId);
     int i = 0;
     String recipients = "";
-    
-	for (User user : users) {
-		if (i != users.size() - 1) {
-			recipients += user.getEmail() + ", ";
-		} else {
-			recipients += user.getEmail();
-		}
-		i++;
-	}
-      Map<String, String> map = new HashMap<>();
-      map.put("email_to", recipients);
-      map.put("email_title", project.getName());
-      map.put("email_content", project.getDescription());
-      rabbitTemplate.convertAndSend(RabbitMqConfig.MESSAGE_QUEUE, map);
+
+    for (User user : users) {
+      if (i != users.size() - 1) {
+        recipients += user.getEmail() + ", ";
+      } else {
+        recipients += user.getEmail();
+      }
+      i++;
+    }
+    Map<String, String> map = new HashMap<>();
+    map.put("email_to", recipients);
+    map.put("email_title", project.getName());
+    map.put("email_content", project.getDescription());
+    rabbitTemplate.convertAndSend(RabbitMqConfig.MESSAGE_QUEUE, map);
   }
-  
-   public void sendToAdmin()
-   {
-	   List<User>users=userRepository.findAll();
-	   int i=0;
-	   String recipients="";
-	   for(User user:users)
-	   {  
-		   if(i!=users.size()-1)
-	   {		   
-		 List<Role>roles=user.getRoles();
-		   for(Role role:roles) {
-		   if(role.getName().equals("ADMIN"))
-		   {
-			   recipients += user.getEmail() + ", ";
-			 
-		   }
-		   }
-	   }
-		   else
-		   {
-			   List<Role>roles=user.getRoles();
-			   for(Role role:roles) {
-			   if(role.getName().equals("ADMIN"))
-			   {
-				   recipients += user.getEmail();
-				 
-			   }
-			   }
-			   
-		   }
-	   }
-	   Map<String, String> map = new HashMap<>();
-	      map.put("email_to", recipients);
-	      map.put("email_title","New User Registered");
-	      map.put("email_content", "I am New, Please Assign me to some Project");
-	      rabbitTemplate.convertAndSend(RabbitMqConfig.MESSAGE_QUEUE, map);
-   }
+
+  public void sendToAdmin() {
+    List<User> users = userRepository.findAll();
+    int i = 0;
+    String recipients = "";
+    for (User user : users) {
+      if (i != users.size() - 1) {
+        List<Role> roles = user.getRoles();
+        for (Role role : roles) {
+          if (role.getName().equals("ADMIN")) {
+            recipients += user.getEmail() + ", ";
+
+          }
+        }
+      } else {
+        List<Role> roles = user.getRoles();
+        for (Role role : roles) {
+          if (role.getName().equals("ADMIN")) {
+            recipients += user.getEmail();
+
+          }
+        }
+
+      }
+    }
+    Map<String, String> map = new HashMap<>();
+    map.put("email_to", recipients);
+    map.put("email_title", "New User Registered");
+    map.put("email_content", "I am New, Please Assign me to some Project");
+    rabbitTemplate.convertAndSend(RabbitMqConfig.MESSAGE_QUEUE, map);
+  }
 
 }
