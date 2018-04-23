2cd1b1fbd2349303a1c03db063ad52778b52fc29
lwinmoeaung <ms.lwinmoeaung@gmail.com>
2018-04-22 23:30:34 -0500

    LMA: Commit for spring email send to users.

--- src/main/java/edu/mum/cs544/project/config/MessageListener.java
+++ src/main/java/edu/mum/cs544/project/config/MessageListener.java
@@ -2,14 +2,40 @@
 * Author: DatDoan
 * Created Date: Apr 21, 2018
 */
+
+
 package edu.mum.cs544.project.config;
 
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
+/**
+* Author: Lwin Moe Aung
+* Modified Date: Apr 22, 2018
+* Comment: Modified receiveMessage() to send email.
+*/
+
 @Component
-public class MessageListener {
-  public void receiveMessage(Map<String, String> message) {
-    System.out.println("Received <" + message + ">");
-  }
+public class MessageListener {	
+	
+	@Autowired
+	public JavaMailSender emailSender;
+
+	public void receiveMessage(Map<String, String> message) {
+		String strarray[] = message.get("email_to").split(",");
+		SimpleMailMessage msg = new SimpleMailMessage();
+		try {
+			msg.setTo(strarray);
+			msg.setSubject(message.get("email_title"));
+			msg.setText(message.get("email_content"));
+			emailSender.send(msg);
+		} catch (MailException exception) {
+			exception.printStackTrace();
+		}
+	}
 }
