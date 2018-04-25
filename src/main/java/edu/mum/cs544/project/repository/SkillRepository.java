3143114b99eb3459b80d41842ebf500a154290f4
lwinmoeaung <ms.lwinmoeaung@gmail.com>
2018-04-24 20:50:14 -0500

    Check Skill duplicate name and null validation.

--- src/main/java/edu/mum/cs544/project/repository/SkillRepository.java
+++ src/main/java/edu/mum/cs544/project/repository/SkillRepository.java
@@ -1,10 +1,14 @@
 package edu.mum.cs544.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import edu.mum.cs544.project.model.Skill;
 
 @Repository
 public interface SkillRepository extends JpaRepository<Skill, Integer> {
+	@Query("select distinct s from Skill s where s.name = :name")
+	Skill findByName(@Param("name") String name);
 }
